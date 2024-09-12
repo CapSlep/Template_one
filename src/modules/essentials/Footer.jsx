dde65c179b1a4b77344197de9e98cd19e9b9af5c
CapSlep <caps0307@gmail.com>
2024-09-12 10:43:27 +0300

    mexico update

--- src/modules/essentials/Footer.jsx
+++ src/modules/essentials/Footer.jsx
@@ -4,15 +4,15 @@ export default function Footer() {
     const data = useData(); // Access data from the context
     return (
         <footer className="footer container">
-            <div className="footer__logo-wrapper">
+            {/* <div className="footer__logo-wrapper">
                 <img src="./img/footer-logo.webp" alt="" />
-            </div>
-            {/* <div className="footer__text-box flex-row">
-                <div className="footer__harlan">
-                    <img src="./img/harman-logo.svg" alt="" />
-                </div>
-                <span className="footer__text">{data.footerText}</span>
             </div> */}
+            <div className="footer__text-box flex-row">
+                {/* <div className="footer__harlan">
+                    <img src="./img/harman-logo.svg" alt="" />
+                </div> */}
+                <span className="footer__text">{data.footerText}</span>
+            </div>
         </footer>
     );
 }
