2c4fda7b13eb4f0345646acd972110b5761f3eb2
CapSlep <caps0307@gmail.com>
2024-09-12 12:44:10 +0300

    govno

--- src/modules/content/Description.jsx
+++ src/modules/content/Description.jsx
@@ -1,60 +1,24 @@
 import { useData } from "../../DataContext"; // Import the custom hook
+import Timer from "../utilities/Timer";
 
 export default function Description() {
     const data = useData(); // Access data from the context
     return (
         <section className="description">
-            <h2 className="description__title">
-                {data.description.descriptionTitle}
-            </h2>
-            <p className="descriptin__text">
-                {data.description.descriptionTextMain}
-            </p>
-            <table className="table">
-                {data.description.descriptionTable.map(
-                    (tableElement, index) => {
-                        return (
-                            <tbody
-                                key={index}
-                                className="table__body"
-                                role="rowgroup"
-                            >
-                                <tr className="table__row">
-                                    <th
-                                        scope="col"
-                                        className="table__head"
-                                        role="cell"
-                                        colSpan="2"
-                                    >
-                                        {tableElement.tableTitle}
-                                    </th>
-                                </tr>
-                                {tableElement.tableRows.map(
-                                    (tableRow, index) => {
-                                        return (
-                                            <tr
-                                                className="table__row"
-                                                key={index}
-                                            >
-                                                <th
-                                                    className="table__cell"
-                                                    role="cell"
-                                                    scope="row"
-                                                >
-                                                    {tableRow.rowTitle}
-                                                </th>
-                                                <td className="table__cell">
-                                                    {tableRow.rowText}
-                                                </td>
-                                            </tr>
-                                        );
-                                    }
-                                )}
-                            </tbody>
-                        );
-                    }
-                )}
-            </table>
+            <Timer startTime={3}></Timer>
+            <div className="description__content flex-column">
+                <div className="description__prompt">
+                    <img src="./img/description/icon.png" alt="!" />
+                    {data.description.descriptionPromtOne}
+                </div>
+                <div className="description__prompt">
+                    <img src="./img/description/icon.png" alt="!" />
+                    {data.description.descriptionPromtTwo}
+                </div>
+                <div className="description__image">
+                    <img src="./img/description/img-1.png" alt="" />
+                </div>
+            </div>
         </section>
     );
 }
--- src/modules/essentials/Footer.jsx
+++ src/modules/essentials/Footer.jsx
@@ -3,16 +3,13 @@ import { useData } from "../../DataContext"; // Import the custom hook
 export default function Footer() {
     const data = useData(); // Access data from the context
     return (
-        <footer className="footer container">
+        <footer className="footer">
             <div className="footer__logo-wrapper">
-                <img src="./img/footer-logo.webp" alt="" />
+                <img src="./img/footer/footer-logo.png" alt="" />
             </div>
-            {/* <div className="footer__text-box flex-row">
-                <div className="footer__harlan">
-                    <img src="./img/harman-logo.svg" alt="" />
-                </div>
+            <div className="footer__text-box flex-row">
                 <span className="footer__text">{data.footerText}</span>
-            </div> */}
+            </div>
         </footer>
     );
 }
